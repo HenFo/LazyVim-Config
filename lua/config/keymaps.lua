751a30ce26dc9eda12793282ab8341e0b74a587b
Henry Fock <h.fock@conterra.de>
2026-01-05 11:35:02 +0100

    fix colliding keymaps in vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,25 +10,28 @@ vim.keymap.set({ "n", "x" }, "<leader>h", "^", { desc = "Jump to line end" })
 vim.keymap.set({ "n", "x" }, "<leader>l", "g_", { desc = "Jump to line start" })
 
 vim.keymap.del("n", "<leader>n")
-vim.keymap.set("n", "<leader>Nq", vim.cmd.cprev, { desc = "Previous Quickfix" })
-vim.keymap.set("n", "<leader>nq", vim.cmd.cnext, { desc = "Next Quickfix" })
 
--- diagnostic
-local diagnostic_goto = function(next, severity)
-  return function()
-    vim.diagnostic.jump({
-      count = (next and 1 or -1) * vim.v.count1,
-      severity = severity and vim.diagnostic.severity[severity] or nil,
-      float = true,
-    })
+if not vim.g.vscode then
+  vim.keymap.set("n", "<leader>Nq", vim.cmd.cprev, { desc = "Previous Quickfix" })
+  vim.keymap.set("n", "<leader>nq", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+  -- diagnostic
+  local diagnostic_goto = function(next, severity)
+    return function()
+      vim.diagnostic.jump({
+        count = (next and 1 or -1) * vim.v.count1,
+        severity = severity and vim.diagnostic.severity[severity] or nil,
+        float = true,
+      })
+    end
   end
+  vim.keymap.set("n", "<leader>nd", diagnostic_goto(true), { desc = "Next Diagnostic" })
+  vim.keymap.set("n", "<leader>Nd", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+  vim.keymap.set("n", "<leader>ne", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+  vim.keymap.set("n", "<leader>Ne", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+  vim.keymap.set("n", "<leader>nw", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+  vim.keymap.set("n", "<leader>Nw", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 end
-vim.keymap.set("n", "<leader>nd", diagnostic_goto(true), { desc = "Next Diagnostic" })
-vim.keymap.set("n", "<leader>Nd", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-vim.keymap.set("n", "<leader>ne", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-vim.keymap.set("n", "<leader>Ne", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-vim.keymap.set("n", "<leader>nw", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-vim.keymap.set("n", "<leader>Nw", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
 -- disable s key so that it doesn't initiate insert mode
 vim.keymap.set({ "n", "x" }, "s", "<Nop>")
