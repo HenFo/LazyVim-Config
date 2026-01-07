cf7e385a07ab8a38b6b5658f098bc8cdd73012d6
Henry Fock <h.fock@conterra.de>
2026-01-07 09:52:45 +0100

    add comment pattern for json

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -24,8 +24,6 @@ return {
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
     { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
     { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
-    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
-    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
     { "<c-p>", "<Plug>(YankyPreviousEntry)" },
     { "<c-n>", "<Plug>(YankyNextEntry)" },
   },
