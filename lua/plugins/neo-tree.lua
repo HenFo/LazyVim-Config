896a23822a3940397c3d517545c0f400e773feb5
Henry Fock <h.fock@conterra.de>
2026-03-03 10:23:42 +0100

    auto close neo-tree on file open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,14 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           visible = true,
