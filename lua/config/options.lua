f650f9541124736e3fb13bdffdbd2edbd046fb9e
Henry Fock <h.fock@conterra.de>
2026-03-04 22:38:57 +0100

    add borders to windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,7 @@ vim.g.ai_cmp = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 15
 vim.opt.wrap = false
+vim.opt.winborder = "rounded"
 vim.opt.list = true
 vim.opt.listchars = {
   tab = "» ",
