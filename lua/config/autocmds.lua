cdaa19112346761d8e50bbf9ba4664e96124c797
Henry Fock <h.fock@conterra.de>
2026-03-04 22:15:15 +0100

    close neo-tree on buffer enter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,4 +8,27 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
 
-require("config.json")
+require("config.json-as-jsonc")
+
+local close_neotree_group = vim.api.nvim_create_augroup("close_neotree_on_file_enter", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  group = close_neotree_group,
+  callback = function(args)
+    local buf = args.buf
+    if not vim.api.nvim_buf_is_valid(buf) then
+      return
+    end
+
+    local filetype = vim.bo[buf].filetype
+    local buftype = vim.bo[buf].buftype
+    if filetype == "neo-tree" or buftype ~= "" then
+      return
+    end
+
+    local ok, neotree = pcall(require, "neo-tree.command")
+    if ok then
+      neotree.execute({ action = "close" })
+    end
+  end,
+})
