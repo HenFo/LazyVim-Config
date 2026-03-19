f17e53e98bdbc1f526b6eda734c8aeb931ce98e2
Henry Fock <h.fock@conterra.de>
2026-03-19 23:20:26 +0100

    fix neotree in opened level

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,6 +3,10 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
+        bind_to_cwd = true,
+        follow_current_file = {
+          enabled = true,
+        },
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
