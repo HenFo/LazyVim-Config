a8fad194678fb41d5182c78b4cfa14273186b1be
Henry Fock <h.fock@conterra.de>
2026-03-28 08:59:46 +0100

    set java lsp to vim root folder

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -2,6 +2,22 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     opts = function(_, opts)
+      local launch_cwd = vim.fs.normalize(vim.uv.cwd())
+      local function clamp_to_launch_cwd(path)
+        path = path and vim.fs.normalize(path) or nil
+        if path and (path == launch_cwd or vim.startswith(path, launch_cwd .. "/")) then
+          return path
+        end
+        return launch_cwd
+      end
+
+      local existing_root_dir = opts.root_dir
+      opts.root_dir = function(path)
+        local detected = existing_root_dir and existing_root_dir(path)
+          or vim.fs.root(path, vim.lsp.config.jdtls.root_markers)
+        return clamp_to_launch_cwd(detected)
+      end
+
       local jdtls_java_path = vim.env.JDTLS_JAVA_PATH
       if jdtls_java_path and jdtls_java_path ~= "" then
         local base_cmd = opts.cmd or { vim.fn.exepath("jdtls") }
