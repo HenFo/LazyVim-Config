4439432be3556791a8d37bfdf2c629d4c507df8f
Henry Fock <h.fock@conterra.de>
2026-05-29 12:56:57 +0200

    seperate normal and copilot suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,8 +1,35 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      auto_trigger = false,
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        auto_trigger = false,
+      },
     },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      -- Remove copilot from auto-triggered sources
+      opts.sources = opts.sources or {}
+      if type(opts.sources.default) == "table" then
+        opts.sources.default = vim.tbl_filter(function(s)
+          return s ~= "copilot"
+        end, opts.sources.default)
+      end
+      -- Manually trigger Copilot completions via Alt-c
+      opts.keymap = opts.keymap or {}
+      opts.keymap["<M-c>"] = {
+        function()
+          local blink = require("blink.cmp")
+          if blink.is_visible() then
+            blink.hide()
+          else
+            blink.show({ providers = { "copilot" } })
+          end
+        end,
+      }
+      return opts
+    end,
   },
 }
